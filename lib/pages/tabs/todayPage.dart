1a4566bab9baa4f52a416e6cd2a6f2ba693c0638
riccardo borroni <riccardoborroni21@gmail.com>
2025-09-22 00:18:09 +0200

    tday page lesson call

--- lib/pages/tabs/todayPage.dart
+++ lib/pages/tabs/todayPage.dart
@@ -1,49 +1,49 @@
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:registrov2/api/models/LessonsModel.dart';
 import 'package:registrov2/provider/clientProvider.dart';
 import 'package:registrov2/widget/headerTab.dart';
 
-class Todaypage extends ConsumerWidget {
-  const Todaypage({super.key});
+class TodayPage extends ConsumerWidget {
+  const TodayPage({super.key});
+
+  Future<List<LessonModel>> fetchLessons(WidgetRef ref) async {
+    final client = ref.watch(clientProvider);
+    return await client.fetchLessons("20250917"); 
+  }
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    List<Map<String, dynamic>> agenda = [];
-
-    var loading = true;
-
-    Future<void> fetchLessons() async {
-      final client = ref.watch(clientProvider);
-      final lessons = await client.fetchLessons("20250917");
-
-      agenda = lessons.map((lesson) => lesson.toJson()).toList();
-
-      loading = false;
-    }
-
-    fetchLessons();
-
     return CupertinoPageScaffold(
       backgroundColor: CupertinoColors.secondarySystemBackground,
       child: SafeArea(
-        child: loading == true
-            ? const Center(child: CupertinoActivityIndicator())
-            : Column(
+        child: FutureBuilder<List<LessonModel>>(
+          future: fetchLessons(ref),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(child: CupertinoActivityIndicator());
+            } else if (snapshot.hasError) {
+              return Center(child: Text('Errore: ${snapshot.error}'));
+            } else {
+              final lessons = snapshot.data ?? [];
+              return Column(
                 children: [
-                  Headertab(title: "Today"),
+                  const Headertab(title: "Today"),
                   Expanded(
                     child: ListView(
-                      children: agenda
-                          .map(
-                            (physics) =>
-                                Text(physics["subjectDesc"].toString()),
-                          )
+                      children: lessons
+                          .map((lesson) => Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Text(lesson.subjectDesc),
+                              ))
                           .toList(),
                     ),
                   ),
                 ],
-              ),
+              );
+            }
+          },
+        ),
       ),
     );
   }
